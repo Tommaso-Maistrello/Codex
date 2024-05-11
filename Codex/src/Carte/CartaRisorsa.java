4c6e6cdc6a2757e1308c30d23677917dac623890
qmuhammad-dragonwarrior <167224797+qmuhammad-dragonwarrior@users.noreply.github.com>
2024-05-11 13:25:23 +0200

    metodo mescola carte funzionante

--- Codex/src/Carte/CartaRisorsa.java
+++ Codex/src/Carte/CartaRisorsa.java
@@ -4,8 +4,10 @@ import java.io.File;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -35,7 +37,7 @@ public class CartaRisorsa extends Carta {
 		return punti;
 	}
 	
-public static  void costruisciCarteRisorsa() throws FileNotFoundException{
+public static  CartaRisorsa[] costruisciCarteRisorsa() throws FileNotFoundException{
 	 String topLeft, topRight, bottomLeft, bottomRight, colore, risorsa;
 	 int punti, id;	 
 	
@@ -70,45 +72,80 @@ public static  void costruisciCarteRisorsa() throws FileNotFoundException{
 			 
 			 if ("".equals(topLeft)) {
 				 topLeft = null;
+				 
 			 }
 		
-			 if ("".equals(topLeft)) {
-				 topLeft = null;
+			 if ("".equals(topRight)) {
+				 topRight = null;
+				 
 			 } if ("".equals(bottomLeft)) {
-				 topLeft = null;
-			 } if ("".equals(bottomRight)) {
 				 bottomLeft = null;
+			 } if ("".equals(bottomRight)) {
+				 bottomRight = null;
 			 }
 			 
 			 cartaRisorsa[i]= new CartaRisorsa(id,topLeft,topRight,bottomLeft,bottomRight,"vuoto","vuoto","vuoto","vuoto",risorsa,colore,punti);
 		//System.out.println(id + topLeft+topRight+bottomLeft+bottomRight+"vuoto"+"vuoto"+"vuoto"+"vuoto"+risorsa+colore+punti );// codice di debug
 			// System.out.println("il top left è " + cartaRisorsa[i].getId());
-			 i++;
+			 
+	//	 System.out.println(id+" "+ cartaRisorsa[i].getId()+" "+cartaRisorsa[i].getAngoloFronteTopLeft());	
 		 
-		
+		 i++;
 	}		
 	
 	leggi.close();
-	
+	return cartaRisorsa;
 	
 	  
 	}
 	public static void main(String[] args) throws FileNotFoundException {
-		costruisciCarteRisorsa();
-	
-	 
+		CartaRisorsa[] mazzo = new CartaRisorsa[40];  
+		mazzo= costruisciCarteRisorsa();
+		 mazzo= mescolaMazzo();
+		 System.out.println("fine");
+			for (int j = 1; j<20;j++) {
+				System.out.println( +mazzo[j].getId()+" "+mazzo[j].getAngoloFronteTopLeft());	
+			//	  System.out.println(Arrays(listaCarte.get(j)));	
+			}
 		
 	}
-	public static  void mescolaMazzo(){
-		List<CartaRisorsa> mazzo = new ArrayList<>();
-	      Collections.shuffle(mazzo);
+	public static  CartaRisorsa[] mescolaMazzo() throws FileNotFoundException{
+		CartaRisorsa[] mazzo = new CartaRisorsa[40];  
+		CartaRisorsa[] temp = new CartaRisorsa[40]; 
+		mazzo= costruisciCarteRisorsa();
+		Random rnd = new Random();
+		int posizioneRandom;
+		
+	for (int i = 19; i>0;i--) {
+		posizioneRandom=rnd.nextInt(i );
+		posizioneRandom+=1; // il numero random viene generato da 0 a 19, non esistono elementi nella posizine 0 quindi aggiungiamo 1
+		
+		temp[i]=mazzo[posizioneRandom];
+		mazzo[posizioneRandom]= mazzo[i];
+		mazzo[i]=temp[i];
+		
+		
+		//System.out.println(posizioneRandom+"id random"+temp[i].getId()+"id mazzo"+mazzo[i].getId());
+		System.out.println(i+"posizione random"+posizioneRandom);
+		//	mazzo[posizioneRandom].equals(mazzo[i]);
+		//mazzo[i].equals(temp);
+				
+		// System.out.println( mazzo[i].getId()+" "+mazzo[i].getAngoloFronteTopLeft());	
+	}
 
+	
+//	ArrayList<CartaRisorsa[]> listaCarte = new ArrayList<>();
+	//  Collections.(listaCarte,mazzo);
+	  //Collections.shuffle(listaCarte);
+	
 	//	  for (int j =1; j<10; j++) {
 			  
 	//		  System.out.println("il top left è " + cartaRisorsa[j].getId());
 			  
 	//	  }
+		return mazzo;
 	}
+	
 	
 }
 
